ba95420659922829faf27767cac76930b2672281
Eric Zarnosky <eric.zarnosky+github@gmail.com>
2025-09-26 15:18:28 +0300

    Update vcenterclass.ps1

--- vcenterclass.ps1
+++ vcenterclass.ps1
@@ -8,41 +8,57 @@ class VsHost {
     [string] $PowerState
     [string] $Manufacturer
     [string] $Model
-    [string] $CPUType        # ProcessorType, with (R) stripped
-    [int]    $CPUs           # NumCPU
-    [string] $MemoryGB       # MemoryTotalGB formatted "{0:N0}"
+    [string] $CPUType
+    [int]    $CPUs
+    [string] $MemoryGB
     [string] $Version
     [string] $Build
     [string] $Timezone
+    [System.Collections.Generic.List[string]] $DataStores = [System.Collections.Generic.List[string]]::new()
     VsHost() {}
 }
 
-# ----- VM class (Snapshots → SnapshotCount + SnapshotSizeGB) -----
+# ----- VM class -----
 class VsVirtualMachine {
     [string] $Name
     [string] $PowerState
     [int]    $CPUs
     [string] $MemoryGB
     [string] $CurrentHost
-    [int]    $SnapshotCount        # total number of snapshots for this VM
-    [string] $SnapshotSizeGB       # total snapshot size (formatted "{0:N2}")
-    [string] $UsedSpaceGB          # formatted "{0:N2}"
-    [string] $ProvisionedSpaceGB   # formatted "{0:N2}"
-    [string] $PercentFree          # formatted "{0:P2}" = Used / Provisioned (per your spec)
+    [int]    $SnapshotCount
+    [string] $SnapshotSizeGB
+    [string] $UsedSpaceGB
+    [string] $ProvisionedSpaceGB
+    [string] $PercentFree
     [string] $CreationDate
     [string] $Notes
+    [string] $HomeDatastorePath
+    [System.Collections.Generic.List[string]] $DiskPaths = [System.Collections.Generic.List[string]]::new()
     VsVirtualMachine() {}
 }
 
 # ----- Snapshot class -----
 class VsSnapshot {
     [string] $VM
-    [string] $SizeGB         # formatted "{0:N2}"
+    [string] $SizeGB
     [string] $Created
     [string] $PowerState
     [bool]   $IsCurrent
     [bool]   $Quiesced
     VsSnapshot() {}
+}
+
+# ----- Datastore class -----
+class VsDataStore {
+    [string] $Name
+    [string] $State
+    [bool]   $Accessible
+    [string] $FreeSpaceGB
+    [string] $CapacityGB
+    [string] $PercentFree
+    [string] $Type
+    [string] $FSVersion
+    VsDataStore() {}
 }
 
 # ----- Cluster class -----
@@ -55,9 +71,9 @@ class VsCluster {
     [string] $VsanDiskClaimMode
     [string] $EVCMode
 
-    [System.Collections.Generic.List[VsHost]]             $Hosts            = [System.Collections.Generic.List[VsHost]]::new()
-    [System.Collections.Generic.List[VsVirtualMachine]]   $VirtualMachines  = [System.Collections.Generic.List[VsVirtualMachine]]::new()
-    [System.Collections.Generic.List[VsSnapshot]]         $Snapshots        = [System.Collections.Generic.List[VsSnapshot]]::new()
+    [System.Collections.Generic.List[VsHost]]            $Hosts           = [System.Collections.Generic.List[VsHost]]::new()
+    [System.Collections.Generic.List[VsVirtualMachine]]  $VirtualMachines = [System.Collections.Generic.List[VsVirtualMachine]]::new()
+    [System.Collections.Generic.List[VsSnapshot]]        $Snapshots       = [System.Collections.Generic.List[VsSnapshot]]::new()
 
     VsCluster() {}
 }
@@ -80,6 +96,7 @@ class VsVCenter {
     [string] $SessionId
 
     [System.Collections.Generic.List[VsDataCenter]] $DataCenters = [System.Collections.Generic.List[VsDataCenter]]::new()
+    [System.Collections.Generic.List[VsDataStore]]  $DataStores  = [System.Collections.Generic.List[VsDataStore]]::new()
 
     VsVCenter() {}
 
@@ -153,7 +170,46 @@ class VsVCenter {
                             $tz = ($h | Select-Object -ExpandProperty Timezone -ErrorAction SilentlyContinue)
                             if ($tz) { $vh.Timezone = [string]$tz } else { $vh.Timezone = '' }
                         }
+
+                        # Host-level datastores
+                        $hostDS = $h | Get-Datastore -ErrorAction SilentlyContinue
+                        foreach ($ds in $hostDS) {
+                            if (-not $vh.DataStores.Contains($ds.Name)) {
+                                [void]$vh.DataStores.Add([string]$ds.Name)
+                            }
+                        }
+
                         [void]$cl.Hosts.Add($vh)
+                    }
+                }
+            }
+        }
+    }
+
+    [void] LoadSnapshots() {
+        foreach ($dc in $this.DataCenters) {
+            $dcObj = Get-Datacenter -Server $this.Name -Name $dc.Name -ErrorAction SilentlyContinue
+            if (-not $dcObj) { continue }
+
+            foreach ($cl in $dc.Clusters) {
+                $clObj = Get-Cluster -Server $this.Name -Name $cl.Name -Location $dcObj -ErrorAction SilentlyContinue
+                if (-not $clObj) { continue }
+
+                $vmObjs = Get-VM -Server $this.Name -Location $clObj -ErrorAction SilentlyContinue
+                foreach ($vmObj in $vmObjs) {
+                    try {
+                        $snaps = $vmObj | Get-Snapshot -ErrorAction Stop
+                    } catch { continue }
+
+                    foreach ($s in $snaps) {
+                        $snap = [VsSnapshot]::new()
+                        $snap.VM         = [string]$vmObj.Name
+                        $snap.SizeGB     = ('{0:N2}' -f [double]$s.SizeGB)
+                        $snap.Created    = [string]$s.Created
+                        $snap.PowerState = [string]$s.PowerState
+                        $snap.IsCurrent  = [bool]$s.IsCurrent
+                        $snap.Quiesced   = [bool]$s.Quiesced
+                        $cl.Snapshots.Add($snap) | Out-Null
                     }
                 }
             }
@@ -188,49 +244,45 @@ class VsVCenter {
                         $create = $vmObj | Select-Object -ExpandProperty CreateDate -ErrorAction SilentlyContinue
                         if (-not $create) { $create = $vmObj.ExtensionData.Config.CreateDate }
                         $vm.CreationDate = if ($create) { [string]$create } else { '' }
-
                         $vm.Notes = [string]$vmObj.Notes
 
-                        # SnapshotCount + SnapshotSizeGB (aggregated from cluster-level snapshots)
-                        $vmSnaps   = $cl.Snapshots | Where-Object { $_.VM -eq $vmObj.Name }
+                        # Snapshot aggregation
+                        $vmSnaps = $cl.Snapshots | Where-Object { $_.VM -eq $vmObj.Name }
                         $vm.SnapshotCount = ($vmSnaps | Measure-Object).Count
                         $totalSize = ($vmSnaps | Measure-Object -Property SizeGB -Sum).Sum
-                        if ($totalSize) { $vm.SnapshotSizeGB = ('{0:N2}' -f [double]$totalSize) } else { $vm.SnapshotSizeGB = "0.00" }
+                        $vm.SnapshotSizeGB = if ($totalSize) { '{0:N2}' -f [double]$totalSize } else { '0.00' }
 
-                        [void]$cl.VirtualMachines.Add($vm)
+                        # Datastore paths
+                        $vm.HomeDatastorePath = [string]$vmObj.ExtensionData.Config.Files.VmPathName
+                        $vm.DiskPaths.Clear()
+                        $hardDisks = $vmObj | Get-HardDisk -ErrorAction SilentlyContinue
+                        foreach ($hd in $hardDisks) {
+                            if ($hd.Filename) { [void]$vm.DiskPaths.Add([string]$hd.Filename) }
+                        }
+
+                        $cl.VirtualMachines.Add($vm) | Out-Null
                     }
                 }
             }
         }
     }
 
-    [void] LoadSnapshots() {
-        foreach ($dc in $this.DataCenters) {
-            $dcObj = Get-Datacenter -Server $this.Name -Name $dc.Name -ErrorAction SilentlyContinue
-            if (-not $dcObj) { continue }
-
-            foreach ($cl in $dc.Clusters) {
-                $clObj = Get-Cluster -Server $this.Name -Name $cl.Name -Location $dcObj -ErrorAction SilentlyContinue
-                if (-not $clObj) { continue }
-
-                $vmObjs = Get-VM -Server $this.Name -Location $clObj -ErrorAction SilentlyContinue
-                foreach ($vmObj in $vmObjs) {
-                    try {
-                        $snaps = $vmObj | Get-Snapshot -ErrorAction Stop
-                    } catch {
-                        continue
-                    }
-                    foreach ($s in $snaps) {
-                        $snap = [VsSnapshot]::new()
-                        $snap.VM         = [string]$vmObj.Name
-                        $snap.SizeGB     = ('{0:N2}' -f [double]$s.SizeGB)
-                        $snap.Created    = [string]$s.Created
-                        $snap.PowerState = [string]$s.PowerState
-                        $snap.IsCurrent  = [bool]$s.IsCurrent
-                        $snap.Quiesced   = [bool]$s.Quiesced
-                        [void]$cl.Snapshots.Add($snap)
-                    }
-                }
+    [void] LoadDataStores() {
+        $dsObjs = Get-Datastore -Server $this.Name -ErrorAction SilentlyContinue
+        foreach ($ds in $dsObjs) {
+            if (-not ($this.DataStores | Where-Object { $_.Name -eq $ds.Name })) {
+                $dso = [VsDataStore]::new()
+                $dso.Name        = $ds.Name
+                $dso.State       = [string]$ds.State
+                $dso.Accessible  = [bool]$ds.ExtensionData.Summary.Accessible
+                $dso.FreeSpaceGB = ('{0:N2}' -f ($ds.FreeSpaceGB))
+                $dso.CapacityGB  = ('{0:N2}' -f ($ds.CapacityGB))
+                if ($ds.CapacityGB -gt 0) {
+                    $dso.PercentFree = ('{0:P2}' -f ($ds.FreeSpaceGB / $ds.CapacityGB))
+                } else { $dso.PercentFree = '' }
+                $dso.Type        = [string]$ds.Type
+                $dso.FSVersion   = [string]$ds.FileSystemVersion
+                $this.DataStores.Add($dso) | Out-Null
             }
         }
     }
@@ -240,13 +292,8 @@ class VsVCenter {
 # Driver code
 # =========================
 
-# Example: list of vCenter names you are connected to
 $names = @('vcsa01.domain.tld','vcsa02.domain.tld')
-
-# Get VIServer objects (assumes you have connected with Connect-VIServer)
 $viServers = Get-VIServer -Server $names
-
-# Strongly typed list of vCenters
 $vcenters = New-Object 'System.Collections.Generic.List[VsVCenter]'
 
 foreach ($vi in $viServers) {
@@ -254,59 +301,8 @@ foreach ($vi in $viServers) {
     $obj.LoadDataCenters()
     $obj.LoadClusters()
     $obj.LoadHosts()
-    $obj.LoadSnapshots()        # populate snapshots first...
-    $obj.LoadVirtualMachines()  # ...then VMs can aggregate SnapshotCount/SizeGB
+    $obj.LoadSnapshots()
+    $obj.LoadVirtualMachines()
+    $obj.LoadDataStores()
     $vcenters.Add($obj) | Out-Null
 }
-
-# Example flattened output blocks (optional):
-
-# # VMs
-# $vcenters | ForEach-Object {
-#     $vc = $_
-#     foreach ($dc in $vc.DataCenters) {
-#         foreach ($cl in $dc.Clusters) {
-#             foreach ($vm in $cl.VirtualMachines) {
-#                 [pscustomobject]@{
-#                     vCenter            = $vc.Name
-#                     DataCenter         = $dc.Name
-#                     Cluster            = $cl.Name
-#                     VM                 = $vm.Name
-#                     PowerState         = $vm.PowerState
-#                     CPUs               = $vm.CPUs
-#                     MemoryGB           = $vm.MemoryGB
-#                     'Current Host'     = $vm.CurrentHost
-#                     SnapshotCount      = $vm.SnapshotCount
-#                     SnapshotSizeGB     = $vm.SnapshotSizeGB
-#                     UsedSpaceGB        = $vm.UsedSpaceGB
-#                     ProvisionedSpaceGB = $vm.ProvisionedSpaceGB
-#                     PercentFree        = $vm.PercentFree
-#                     CreationDate       = $vm.CreationDate
-#                     Notes              = $vm.Notes
-#                 }
-#             }
-#         }
-#     }
-# } | Format-Table -Auto
-
-# # Snapshots
-# $vcenters | ForEach-Object {
-#     $vc = $_
-#     foreach ($dc in $vc.DataCenters) {
-#         foreach ($cl in $dc.Clusters) {
-#             foreach ($s in $cl.Snapshots) {
-#                 [pscustomobject]@{
-#                     vCenter   = $vc.Name
-#                     DataCenter= $dc.Name
-#                     Cluster   = $cl.Name
-#                     VM        = $s.VM
-#                     SizeGB    = $s.SizeGB
-#                     Created   = $s.Created
-#                     PowerState= $s.PowerState
-#                     IsCurrent = $s.IsCurrent
-#                     Quiesced  = $s.Quiesced
-#                 }
-#             }
-#         }
-#     }
-# } | Format-Table -Auto
